34c9a86b77a628357241e0e19507fe9e02121c05
nescheytt <nahuel.scheytt@gmail.com>
2023-09-07 13:07:21 +0100

    feat: create types

---
+++ types/ticket.ts
@@ -0,0 +1,18 @@
+export type Ticket = {
+  order_Date: string;
+  order_ID: string;
+  order_Status: string;
+  variation_Name: string;
+  ticket_ID: string;
+  ticket_Price: string;
+  admission_Status: string;
+  attendee_Name: string;
+  attendee_LastName: string;
+  attendee_Email: string;
+  attendee_Telephone: string;
+  purchaser_FirstName: string;
+  purchaser_LastName: string;
+  purchaser_Email: string;
+  purchaser_Phone: string;
+  created_Manually: string;
+};
