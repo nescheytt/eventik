949a76ebf1217b7d9ae18654775c618f3c2c470f
Nahuel Scheytt <nahuel.scheytt@gmail.com>
2023-09-07 15:55:48 +0100

    [Feat] Table actions (#3)
    * feat: add button actions
    
    * feat: add w-fit dropdown filters

--- components/data-table-row-actions.tsx
+++ components/data-table-row-actions.tsx
@@ -1,43 +1,25 @@
 "use client"
 
-import { DotsHorizontalIcon } from "@radix-ui/react-icons"
+import { PinBottomIcon, PaperPlaneIcon } from "@radix-ui/react-icons"
 import { Row } from "@tanstack/react-table"
 import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
 
 interface DataTableRowActionsProps<TData> {
   row: Row<TData>
 }
 
 export function DataTableRowActions<TData>({ row }: DataTableRowActionsProps<TData>) {
-  const task = row;
-
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="flex h-8 w-8 p-0 data-[state=open]:bg-muted">
-          <DotsHorizontalIcon className="h-4 w-4" />
-          <span className="sr-only">Open menu</span>
-        </Button>
-      </DropdownMenuTrigger>
+    <div className="flex">
+      <Button variant="ghost" className="flex h-8 w-8 p-0 data-[state=open]:bg-muted">
+        <PaperPlaneIcon className="h-4 w-4 -rotate-45" />
+        <span className="sr-only">Download table</span>
+      </Button>
 
-      <DropdownMenuContent align="end" className="w-[160px]">
-        <DropdownMenuItem>Edit</DropdownMenuItem>
-        <DropdownMenuItem>Make a copy</DropdownMenuItem>
-        <DropdownMenuItem>Favorite</DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>
-          Delete
-          <DropdownMenuShortcut>⌘⌫</DropdownMenuShortcut>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+      <Button variant="ghost" className="flex h-8 w-8 p-0 data-[state=open]:bg-muted">
+        <PinBottomIcon className="h-4 w-4" />
+        <span className="sr-only">Download table</span>
+      </Button>
+    </div>
   )
 }
