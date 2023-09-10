9ac1a3727ca6f3190e7de9d1f69f7b9da57fa070
nescheytt <nahuel.scheytt@gmail.com>
2023-09-10 20:33:02 +0100

    feat: add new style button columnas

--- components/data-table-view-options.tsx
+++ components/data-table-view-options.tsx
@@ -27,7 +27,7 @@ export function DataTableViewOptions<TData>({
         <Button
           variant="outline"
           size="sm"
-          className="ml-auto hidden h-8 lg:flex"
+          className="ml-auto hidden h-9 lg:flex"
         >
           <MixerHorizontalIcon className="mr-2 h-4 w-4" />
           Columnas
