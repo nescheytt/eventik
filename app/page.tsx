21747a8eb795eede46866fe7e720ada16392472b
nescheytt <nahuel.scheytt@gmail.com>
2023-09-15 15:49:47 +0100

    refactor: change page app const + types

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,20 @@
 'use client'
 
+import type { EventMagicTicket } from '@/types/ticket'
 import { useSuspenseQuery } from '@apollo/experimental-nextjs-app-support/ssr'
 import { GET_TICKETS } from '@/lib/queries'
-import { GET_TICKETS_NEW } from '@/lib/queries-new'
 import HeroTitle from '@/components/hero-title'
 import DataTable from '@/components/data-table'
 import TicketStatusOverview from '@/components/ticket-status-overview'
-import { TypeDefinitionNode } from 'graphql'
-import type { EventMagicTicket, TicketNew } from '@/types/ticket'
 
 const NEXT_PUBLIC_EVENT_PRODUCT_ID = parseInt(process.env.NEXT_PUBLIC_EVENT_PRODUCT_ID!)
 
-
-
 export default function TicketPage() {
-  const { error, data } = useSuspenseQuery<EventMagicTicket>(GET_TICKETS_NEW, {
+  const { error, data } = useSuspenseQuery<EventMagicTicket>(GET_TICKETS, {
     variables: { product_id: NEXT_PUBLIC_EVENT_PRODUCT_ID }
   })
 
   const tickets = data?.eventMagicTickets || []
-
-  console.log(tickets)
 
   if (error) return <div>Error</div>
 
