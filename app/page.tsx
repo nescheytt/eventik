49197ee858c20c15a8aaff7c11d10f7ee65a0d6f
nescheytt <nahuel.scheytt@gmail.com>
2023-09-22 17:03:33 +0100

    feat: apply apollo-wrapper with use-suspense-query

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,30 @@
+"use client"
+
+import { Suspense, useState } from "react"
+import { ErrorBoundary } from "react-error-boundary"
 import { HeroTitle } from "@/components/hero-title"
 import { DataTable } from "@/components/data-table"
 import { TicketStatusOverview } from "@/components/ticket-status-overview"
 import { columns } from "@/components/columns"
-import { Tickets } from "@/services/tickets"
+import { useSuspenseQuery } from "@apollo/experimental-nextjs-app-support/ssr"
+import { GET_TICKETS } from "@/lib/queries"
 
-export default async function TicketPage() {
-  const data = await Tickets()
+export default function TicketPage() {
+  const [selectedProduct, setSelectedProduct] = useState(253);
+
+  const { data } = useSuspenseQuery(GET_TICKETS, {
+    variables: { product_id: selectedProduct }
+  })
 
   return (
-    <main className="container flex flex-col gap-y-4 px-4 pb-12 pt-4">
-      <HeroTitle />
-      <TicketStatusOverview tickets={data} />
-      <DataTable data={data} columns={columns} />
-    </main>
+    <ErrorBoundary fallback={<div>Algo salió mal</div>}>
+      <Suspense fallback={<div>Cargando...</div>}>
+        <main className="container flex flex-col gap-y-4 px-4 pb-12 pt-4">
+          <HeroTitle selectedProduct={selectedProduct} setSelectedProduct={setSelectedProduct} />
+          <TicketStatusOverview tickets={data.eventMagicTickets} />
+          <DataTable data={data.eventMagicTickets} columns={columns} />
+        </main>
+      </Suspense>
+    </ErrorBoundary>
   )
 }
